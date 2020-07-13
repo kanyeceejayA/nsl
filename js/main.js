134db38d55ff4602d2176047874e4927eadf06b2
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2020-07-13 11:49:26 +0300

    fix menu

--- js/main.js
+++ js/main.js
@@ -90,3 +90,47 @@ $(window).on('load', function() {
 
 })(jQuery);
 
+// Scrol to top button
+
+let mybutton = document.getElementById("TopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+	mybutton.style.display = "block";
+  } else {
+	mybutton.style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $('body, html').animate({scrollTop: 0}, 'EaseinOutQuad');
+}
+
+ //smooth scroll
+  // handle links with @href started with '#' only
+  $(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+    
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos},'slow','easeInOutQuad');
+  });
+
